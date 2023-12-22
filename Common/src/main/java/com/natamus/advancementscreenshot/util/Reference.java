d76bafd230eb02a5cf2b7ecd082ffdf6a3a9454e
Rick South <rick@natamus.com>
2023-12-22 18:24:29 +0100

    (v4.7) Moved mixin files to the Common source folder to make future development easier.

--- Common/src/main/java/com/natamus/advancementscreenshot/util/Reference.java
+++ Common/src/main/java/com/natamus/advancementscreenshot/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.advancementscreenshot.util;
 public class Reference {
 	public static final String MOD_ID = "advancementscreenshot";
 	public static final String NAME = "Advancement Screenshot";
-	public static final String VERSION = "4.6";
+	public static final String VERSION = "4.7";
 	public static final String ACCEPTED_VERSIONS = "[1.20.2]";
 }
