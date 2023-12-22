4dc9b7788987028b6cd4be3340c85f4cddfd48b1
Rick South <rick@natamus.com>
2023-12-22 18:24:19 +0100

    (v4.7) Moved mixin files to the Common source folder to make future development easier.

--- Common/src/main/java/com/natamus/advancementscreenshot/mixin/AdvancementToastMixin.java
+++ Common/src/main/java/com/natamus/advancementscreenshot/mixin/AdvancementToastMixin.java
@@ -1,14 +1,16 @@
-package com.natamus.advancementscreenshot.forge.mixin;
+package com.natamus.advancementscreenshot.mixin;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.natamus.advancementscreenshot.util.Util;
-import net.minecraft.client.gui.components.toasts.AdvancementToast;
-import net.minecraft.client.gui.components.toasts.Toast;
-import net.minecraft.client.gui.components.toasts.ToastComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.natamus.advancementscreenshot.util.Util;
+
+import net.minecraft.client.gui.components.toasts.AdvancementToast;
+import net.minecraft.client.gui.components.toasts.Toast;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
 
 @Mixin(value = AdvancementToast.class, priority = 1001)
 public class AdvancementToastMixin {
