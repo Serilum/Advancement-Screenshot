15d01d07d30aeaaf39f1b5959e71396052cd2d81
Rick South <rick@natamus.com>
2024-04-28 19:04:40 +0200

    (v4.8) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/advancementscreenshot/neoforge/events/NeoForgeAdvancementGetEvent.java
+++ NeoForge/src/main/java/com/natamus/advancementscreenshot/neoforge/events/NeoForgeAdvancementGetEvent.java
@@ -3,18 +3,14 @@ package com.natamus.advancementscreenshot.neoforge.events;
 import com.natamus.advancementscreenshot.data.Constants;
 import com.natamus.advancementscreenshot.events.AdvancementGetEvent;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.neoforge.event.TickEvent.ClientTickEvent;
-import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 
 @EventBusSubscriber(value = Dist.CLIENT)
 public class NeoForgeAdvancementGetEvent {
 	@SubscribeEvent
-	public static void onClientTick(ClientTickEvent e) {
-		if (!e.phase.equals(Phase.END)) {
-			return;
-		}
+	public static void onClientTick(ClientTickEvent.Post e) {
 
 		AdvancementGetEvent.onClientTick(Constants.mc);
 	}
