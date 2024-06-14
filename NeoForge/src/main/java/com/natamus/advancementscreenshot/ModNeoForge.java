acf39aba40cb9a45b2981194ad7514e92e38f562
Rick South <rick@natamus.com>
2024-06-14 05:45:48 +0200

    Advancement Screenshot source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/advancementscreenshot/ModNeoForge.java
@@ -0,0 +1,40 @@
+package com.natamus.advancementscreenshot;
+
+import com.natamus.advancementscreenshot.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.advancementscreenshot.neoforge.events.NeoForgeAdvancementGetEvent;
+import com.natamus.advancementscreenshot.util.Reference;
+import com.natamus.collective.check.RegisterMod;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		if (!FMLEnvironment.dist.equals(Dist.CLIENT)) {
+			return;
+		}
+
+		NeoForge.EVENT_BUS.register(NeoForgeAdvancementGetEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
