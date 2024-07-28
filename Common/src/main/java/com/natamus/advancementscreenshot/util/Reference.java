c4ef8f3bb276263898083e75b4ceb7b2a9d92c5a
Rick South <rick@natamus.com>
2024-07-28 07:45:42 +0200

    (v4.9) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/advancementscreenshot/util/Reference.java
+++ Common/src/main/java/com/natamus/advancementscreenshot/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.advancementscreenshot.util;
 public class Reference {
 	public static final String MOD_ID = "advancementscreenshot";
 	public static final String NAME = "Advancement Screenshot";
-	public static final String VERSION = "4.8";
+	public static final String VERSION = "4.9";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
