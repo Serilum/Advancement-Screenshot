7743ce4f265a7d6a9cd17e06a7f179b3c1a8e1b2
Rick South <rick@natamus.com>
2025-04-05 15:36:39 +0200

    (v5.1) (1.21.5) Fixed the mod not taking screenshots correctly.

--- Common/src/main/java/com/natamus/advancementscreenshot/util/Util.java
+++ Common/src/main/java/com/natamus/advancementscreenshot/util/Util.java
@@ -1,10 +1,15 @@
 package com.natamus.advancementscreenshot.util;
 
+import net.minecraft.network.chat.Component;
+
 public class Util {
+	public static Component activeAdvancementTitle = null;
 	public static boolean takescreenshot = false;
 	public static int cooldown = -1;
 	
-	public static void takeScreenshot() {
+	public static void takeScreenshot(Component advancementTitle) {
+		activeAdvancementTitle = advancementTitle;
+
 		takescreenshot = true;
 		cooldown = 20;
 	}
