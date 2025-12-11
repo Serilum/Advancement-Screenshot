e728e4e689c83834aad27c166f2559036dac963a
Rick South <rick@natamus.com>
2025-12-11 03:00:07 +0100

    Advancement Screenshot source code for 1.21.11.

---
+++ NeoForge/src/main/java/com/natamus/advancementscreenshot/neoforge/events/NeoForgeAdvancementGetEvent.java
@@ -0,0 +1,15 @@
+package com.natamus.advancementscreenshot.neoforge.events;
+
+import com.natamus.advancementscreenshot.data.Constants;
+import com.natamus.advancementscreenshot.events.AdvancementGetEvent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeAdvancementGetEvent {
+	@SubscribeEvent
+	public static void onClientTick(ClientTickEvent.Post e) {
+
+		AdvancementGetEvent.onClientTick(Constants.mc);
+	}
+}
